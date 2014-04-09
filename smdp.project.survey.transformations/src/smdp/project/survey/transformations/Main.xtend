b3b2fb5009301a006764bad1ddf057fa8d40f5be
Kristian Andersen <me@kristianandersen.me>
2014-04-09 13:20:56 +0200

    Added transformations project

---
+++ smdp.project.survey.transformations/src/smdp/project/survey/transformations/Main.xtend
@@ -0,0 +1,10 @@
+package smdp.project.survey.transformations
+
+import survey.SurveyPackage
+import org.eclipse.emf.ecore.EObject;
+
+class Main {
+	def static void main(String[] args) {
+		SurveyPackage.eINSTANCE.eClass
+	}
+}
---
+++ smdp.project.survey.transformations/xtend-gen/smdp/project/survey/transformations/Main.java
@@ -0,0 +1,10 @@
+package smdp.project.survey.transformations;
+
+import survey.SurveyPackage;
+
+@SuppressWarnings("all")
+public class Main {
+  public static void main(final String[] args) {
+    SurveyPackage.eINSTANCE.eClass();
+  }
+}
